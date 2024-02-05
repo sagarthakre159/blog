535f7765e35ca2393771f7c7d38173256c8615e9
Sagar Thakre <sagarthakre159.com>
2024-02-05 12:45:26 +0530

    fourth

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,20 +1,9 @@
 import React from 'react'
-async function productList(){
-  let data = await fetch("https://dummyjson.com/products");
-  data= await data.json();
-  return data.products
-}
 
-export default async function page() {
-  let products= await productList();
+function About() {
   return (
-    <div>
-      <h1>Product List</h1>
-      {
-        products.map((items)=>
-        <div><h3>Name:{items.title}</h3></div>
-        )
-      }
-    </div>
+    <div>page</div>
   )
 }
+
+export default About
